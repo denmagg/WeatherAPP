9b8f203983cd865ce6258bd014ff3453b5219f0e
Denis <denmagg.work@gmail.com>
2022-03-23 18:10:42 +0300

    Transferred weather UI elements from MayWeatherController to CityView

--- WeatherAPP/MainWeatherModule/View/CityView.swift
+++ WeatherAPP/MainWeatherModule/View/CityView.swift
@@ -9,20 +9,172 @@ import UIKit
 
 class CityView: UIView {
     
+    let cityLabel = UILabel()
+    let weatherLabel = UILabel()
+    let currentTemperatureLabel = UILabel()
+    let todayDateLabel = UILabel()
+    let todayLabel = UILabel()
+    let todayMaxTemperatureLabel = UILabel()
+    let todayMinTemperatureLabel = UILabel()
+    
+    var collectionViewDataSource: UICollectionViewDataSource?
+    var collectionViewDelegate: UICollectionViewDelegate?
+    var collectionView: UICollectionView?
+    
+    var tableViewDataSource: TableViewDataSource?
+    var tableViewDelegate: TableViewDelegate?
+    let tableView = UITableView()
+    
     init(frame: CGRect, color: UIColor) {
         super.init(frame: frame)
         self.backgroundColor = color
+        
+        configureView()
+        configureMainWeatherInfo()
+        setupSubviews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    private func configureView() {
+        
     }
-    */
+    
+    private func configureMainWeatherInfo() {
+        cityLabel.text = "Moscow"
+        cityLabel.font = .systemFont(ofSize: 32)
+        
+        weatherLabel.text = "Mostly Clear"
+        weatherLabel.font = .systemFont(ofSize: 17)
+        
+        currentTemperatureLabel.text = "5°"
+        currentTemperatureLabel.font = .systemFont(ofSize: 50)
+        
+        todayDateLabel.text = "Friday"
+        todayDateLabel.font = .systemFont(ofSize: 20)
+        
+        todayLabel.text = "TODAY"
+        todayLabel.font = .boldSystemFont(ofSize: 16)
+        
+        todayMinTemperatureLabel.text = "-8"
+        todayMinTemperatureLabel.font = .systemFont(ofSize: 20)
+        
+        todayMaxTemperatureLabel.text = "7"
+        todayMaxTemperatureLabel.font = .systemFont(ofSize: 20)
+        
+        let layout = CollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 100), collectionViewLayout: layout)
+        collectionView?.backgroundColor = .red
+        let dataHourlyForecast = [("Now","☀️","8°"), ("20","☀️","8°"), ("21","☀️","8°"), ("22","☀️","8°"), ("23","☀️","8°"), ("00","☀️","8°"), ("01","☀️","8°"), ("02","☀️","8°"), ("03","☀️","8°"), ("04","☀️","8°"), ("05","☀️","8°"), ("06","☀️","8°"), ("06:28","☀️","Sunrise°"), ("07","☀️","8°"), ("08","☀️","8°"), ("09","☀️","8°"), ("10","☀️","8°"), ("11","☀️","8°"), ("12","☀️","8°"), ("13","☀️","8°"), ("14","☀️","8°"), ("15","☀️","8°"), ("16","☀️","8°"), ("17","☀️","8°"), ("18","☀️","8°"), ("18:48","☀️","Sunset°"), ("19","☀️","8°")]
+        let hourlyWeatherData = HourlyWeatherData(dataHourlyForecast: dataHourlyForecast)
+        self.collectionViewDelegate = CollectionViewDelegate(withDelegate: self)
+        self.collectionViewDataSource = CollectionViewDataSource(withData: hourlyWeatherData)
+        collectionView?.delegate = self.collectionViewDelegate
+        collectionView?.dataSource = self.collectionViewDataSource
+        collectionView?.register(HourlyWeatherCell.self, forCellWithReuseIdentifier: "hourlyWeatherCell")
+        collectionView?.showsHorizontalScrollIndicator = false
+//        collectionView.setCollectionViewLayout(UICollectionViewLayout, animated: true)
+                
+        //FIXME временная data
+        
+        let dataDayForecast = [("Saturday", "☀️", "8", "-1"), ("Sunday", "☀️", "8", "-1"), ("Monday", "☀️", "8", "-1"), ("Tuesday", "☀️", "8", "-1"), ("Wednesday", "☀️", "8", "-1"), ("Thursday", "☀️", "8", "-1"), ("Friday", "☀️", "8", "-1"), ("Saturday", "☀️", "8", "-1"), ("Sunday", "☀️", "8", "-1")]
+        let dataDetailDescription = "Today: Mostly clear currently. It's 5°; the high today was forecast as 9°"
+        let dataSpecs = [("SUNRISE", "6:29"), ("SUNSET", "18:45"), ("CHANCE OF RAIN", "10%"), ("HUMIDITY", "55%"), ("WIND", "NW 3 m/s"), ("FEELS LIKE", "3°"), ("PRECIPITATION", "0 cm"), ("PRESSURE", "775,71 mm Hg"), ("VISIBILITY", "9,7 km"), ("UV INDEX", "0")]
+        let weatherData = WeatherData(dataDayForecast: dataDayForecast, dataDetailDescription: dataDetailDescription, dataSpecs: dataSpecs)
+        
+        self.tableViewDelegate = TableViewDelegate(withDelegate: self)
 
+        self.tableViewDataSource = TableViewDataSource(withData: weatherData)
+        tableView.delegate = self.tableViewDelegate
+        tableView.dataSource = self.tableViewDataSource
+        tableView.backgroundColor = .green
+        tableView.separatorStyle = .none
+        tableView.register(DayForecastTableViewCell.self, forCellReuseIdentifier: "dayForecastCell")
+        tableView.register(DetailDescriptionTableViewCell.self, forCellReuseIdentifier: "detailDescriptionCell")
+        tableView.register(SpecsTableViewCell.self, forCellReuseIdentifier: "specsCell")
+    }
+    
+    private func setupSubviews() {
+        self.addSubview(cityLabel)
+        self.addSubview(weatherLabel)
+        self.addSubview(currentTemperatureLabel)
+        
+        self.addSubview(todayDateLabel)
+        self.addSubview(todayLabel)
+        self.addSubview(todayMinTemperatureLabel)
+        self.addSubview(todayMaxTemperatureLabel)
+        
+        self.addSubview(collectionView!)
+        self.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        // setup cityLabel constraints
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        //cityLabel.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        cityLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+        
+        // setup weatherLabel constraints
+        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
+        weatherLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        weatherLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor).isActive = true
+        
+        // setup currentTemperatureLabel constraints
+        currentTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        currentTemperatureLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        currentTemperatureLabel.topAnchor.constraint(equalTo: weatherLabel.bottomAnchor).isActive = true
+
+        // setup todayDateLabel constraints
+        todayDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayDateLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
+        todayDateLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        todayDateLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 30).isActive = true
+        
+        // setup todayLabel constraints
+        todayLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayLabel.firstBaselineAnchor.constraint(equalTo: todayDateLabel.firstBaselineAnchor).isActive = true
+        //todayLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
+        todayLabel.leadingAnchor.constraint(equalTo: todayDateLabel.trailingAnchor, constant: 10).isActive = true
+        
+        // setup todayMinTemperatureLabel constraints
+        todayMinTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayMinTemperatureLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
+        todayMinTemperatureLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        
+        // setup todayMaxTemperatureLabel constraints
+        todayMaxTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayMaxTemperatureLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
+        todayMaxTemperatureLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -60).isActive = true
+        
+        // setup collectionView constraints
+        collectionView?.translatesAutoresizingMaskIntoConstraints = false
+        collectionView?.topAnchor.constraint(equalTo: todayDateLabel.bottomAnchor, constant: 5).isActive = true
+        collectionView?.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        collectionView?.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        collectionView?.bottomAnchor.constraint(equalTo: tableView.topAnchor).isActive = true
+        
+        // setup tableView constraints
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: todayDateLabel.bottomAnchor, constant: 100).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -34).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+
+}
+
+extension CityView: WeatherTableViewDelegate {
+    func selectedCell(row: Int) {
+        print("Row: \(row)")
+    }
+}
+
+extension CityView: WeatherCollectionViewDelegate {
+    func selectedItem(number: Int) {
+        print("selected item number: \(number)")
+    }
 }
--- WeatherAPP/MainWeatherModule/View/MainWeatherViewController.swift
+++ WeatherAPP/MainWeatherModule/View/MainWeatherViewController.swift
@@ -17,7 +17,13 @@ struct HourlyWeatherData {
     var dataHourlyForecast: [(String, String, String)]
 }
 
-class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, WeatherTableViewDelegate, WeatherCollectionViewDelegate {
+class MainWeatherViewController: UIViewController {
+    
+    var presenter: MainWeatherPresenterProtocol!
+    
+    var bottomPadding: CGFloat?
+    let manageButton = UIButton()
+    let weatherLinkButton = UIButton()
     
     lazy var cityViews = [CityView(frame: CGRect.zero, color: .blue), CityView(frame: CGRect.zero, color: .gray)]
     
@@ -44,42 +50,6 @@ class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, Weat
         return pageControl
     }()
     
-    @objc func pageControlTapHaddler(sender: UIPageControl) {
-        scrollView.scrollTo(horisontalPage: sender.currentPage , animated: true)
-    }
-    
-    func selectedItem(number: Int) {
-        print("selected item number: \(number)")
-    }
-    
-    func selectedCell(row: Int) {
-        print("Row: \(row)")
-    }
-    
-    
-    var presenter: MainWeatherPresenterProtocol!
-    
-    var bottomPadding: CGFloat?
-    
-    let cityLabel = UILabel()
-    let weatherLabel = UILabel()
-    let currentTemperatureLabel = UILabel()
-    let todayDateLabel = UILabel()
-    let todayLabel = UILabel()
-    let todayMaxTemperatureLabel = UILabel()
-    let todayMinTemperatureLabel = UILabel()
-    
-    var collectionViewDataSource: UICollectionViewDataSource?
-    var collectionViewDelegate: UICollectionViewDelegate?
-    var collectionView: UICollectionView?
-    
-    var tableViewDataSource: TableViewDataSource?
-    var tableViewDelegate: TableViewDelegate?
-    let tableView = UITableView()
-    
-    let manageButton = UIButton()
-    let weatherLinkButton = UIButton()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -92,7 +62,6 @@ class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, Weat
         configureMainWeatherInfo()
         setupSubviews()
         setupConstraints()
-        
     }
     
     private func configureView() {
@@ -100,59 +69,6 @@ class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, Weat
     }
     
     private func configureMainWeatherInfo() {
-        cityLabel.text = "Moscow"
-        cityLabel.font = .systemFont(ofSize: 32)
-//        cityLabel.font = UIFont(name: "Geeza Pro", size: 30)
-        
-        weatherLabel.text = "Mostly Clear"
-        weatherLabel.font = .systemFont(ofSize: 17)
-        
-        currentTemperatureLabel.text = "5°"
-        currentTemperatureLabel.font = .systemFont(ofSize: 50)
-        
-        todayDateLabel.text = "Friday"
-        todayDateLabel.font = .systemFont(ofSize: 20)
-        
-        todayLabel.text = "TODAY"
-        todayLabel.font = .boldSystemFont(ofSize: 16)
-        
-        todayMinTemperatureLabel.text = "-8"
-        todayMinTemperatureLabel.font = .systemFont(ofSize: 20)
-        
-        todayMaxTemperatureLabel.text = "7"
-        todayMaxTemperatureLabel.font = .systemFont(ofSize: 20)
-        
-        let layout = CollectionViewFlowLayout()
-        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 100), collectionViewLayout: layout)
-        collectionView?.backgroundColor = .red
-        let dataHourlyForecast = [("Now","☀️","8°"), ("20","☀️","8°"), ("21","☀️","8°"), ("22","☀️","8°"), ("23","☀️","8°"), ("00","☀️","8°"), ("01","☀️","8°"), ("02","☀️","8°"), ("03","☀️","8°"), ("04","☀️","8°"), ("05","☀️","8°"), ("06","☀️","8°"), ("06:28","☀️","Sunrise°"), ("07","☀️","8°"), ("08","☀️","8°"), ("09","☀️","8°"), ("10","☀️","8°"), ("11","☀️","8°"), ("12","☀️","8°"), ("13","☀️","8°"), ("14","☀️","8°"), ("15","☀️","8°"), ("16","☀️","8°"), ("17","☀️","8°"), ("18","☀️","8°"), ("18:48","☀️","Sunset°"), ("19","☀️","8°")]
-        let hourlyWeatherData = HourlyWeatherData(dataHourlyForecast: dataHourlyForecast)
-        self.collectionViewDelegate = CollectionViewDelegate(withDelegate: self)
-        self.collectionViewDataSource = CollectionViewDataSource(withData: hourlyWeatherData)
-        collectionView?.delegate = self.collectionViewDelegate
-        collectionView?.dataSource = self.collectionViewDataSource
-        collectionView?.register(HourlyWeatherCell.self, forCellWithReuseIdentifier: "hourlyWeatherCell")
-        collectionView?.showsHorizontalScrollIndicator = false
-//        collectionView.setCollectionViewLayout(UICollectionViewLayout, animated: true)
-                
-        //FIXME временная data
-        
-        let dataDayForecast = [("Saturday", "☀️", "8", "-1"), ("Sunday", "☀️", "8", "-1"), ("Monday", "☀️", "8", "-1"), ("Tuesday", "☀️", "8", "-1"), ("Wednesday", "☀️", "8", "-1"), ("Thursday", "☀️", "8", "-1"), ("Friday", "☀️", "8", "-1"), ("Saturday", "☀️", "8", "-1"), ("Sunday", "☀️", "8", "-1")]
-        let dataDetailDescription = "Today: Mostly clear currently. It's 5°; the high today was forecast as 9°"
-        let dataSpecs = [("SUNRISE", "6:29"), ("SUNSET", "18:45"), ("CHANCE OF RAIN", "10%"), ("HUMIDITY", "55%"), ("WIND", "NW 3 m/s"), ("FEELS LIKE", "3°"), ("PRECIPITATION", "0 cm"), ("PRESSURE", "775,71 mm Hg"), ("VISIBILITY", "9,7 km"), ("UV INDEX", "0")]
-        let weatherData = WeatherData(dataDayForecast: dataDayForecast, dataDetailDescription: dataDetailDescription, dataSpecs: dataSpecs)
-        
-        self.tableViewDelegate = TableViewDelegate(withDelegate: self)
-
-        self.tableViewDataSource = TableViewDataSource(withData: weatherData)
-        tableView.delegate = self.tableViewDelegate
-        tableView.dataSource = self.tableViewDataSource
-        tableView.backgroundColor = .green
-        tableView.separatorStyle = .none
-        tableView.register(DayForecastTableViewCell.self, forCellReuseIdentifier: "dayForecastCell")
-        tableView.register(DetailDescriptionTableViewCell.self, forCellReuseIdentifier: "detailDescriptionCell")
-        tableView.register(SpecsTableViewCell.self, forCellReuseIdentifier: "specsCell")
-        
         manageButton.setImage(UIImage(systemName: "list.dash"), for: .normal)
         manageButton.tintColor = .init(white: 1, alpha: 0.5)
         
@@ -163,18 +79,6 @@ class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, Weat
     private func setupSubviews() {
         self.view.addSubview(scrollView)
         self.view.addSubview(pageControl)
-        
-        self.view.addSubview(cityLabel)
-        self.view.addSubview(weatherLabel)
-        self.view.addSubview(currentTemperatureLabel)
-        
-        self.view.addSubview(todayDateLabel)
-        self.view.addSubview(todayLabel)
-        self.view.addSubview(todayMinTemperatureLabel)
-        self.view.addSubview(todayMaxTemperatureLabel)
-        
-        self.view.addSubview(collectionView!)
-        self.view.addSubview(tableView)
         
         self.view.addSubview(manageButton)
         self.view.addSubview(weatherLinkButton)
@@ -195,79 +99,21 @@ class MainWeatherViewController: UIViewController, MainWeatherViewProtocol, Weat
         pageControl.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12).isActive = true
         
-        // setup cityLabel constraints
-        cityLabel.translatesAutoresizingMaskIntoConstraints = false
-        cityLabel.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        //cityLabel.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
-        cityLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
-        
-        // setup weatherLabel constraints
-        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
-        weatherLabel.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        weatherLabel.topAnchor.constraint(equalTo: cityLabel.bottomAnchor).isActive = true
-        
-        // setup currentTemperatureLabel constraints
-        currentTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
-        currentTemperatureLabel.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        currentTemperatureLabel.topAnchor.constraint(equalTo: weatherLabel.bottomAnchor).isActive = true
-
-        // setup todayDateLabel constraints
-        todayDateLabel.translatesAutoresizingMaskIntoConstraints = false
-        todayDateLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
-        todayDateLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-        todayDateLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 30).isActive = true
-        
-        // setup todayLabel constraints
-        todayLabel.translatesAutoresizingMaskIntoConstraints = false
-        todayLabel.firstBaselineAnchor.constraint(equalTo: todayDateLabel.firstBaselineAnchor).isActive = true
-        //todayLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
-        todayLabel.leadingAnchor.constraint(equalTo: todayDateLabel.trailingAnchor, constant: 10).isActive = true
-        
-        // setup todayMinTemperatureLabel constraints
-        todayMinTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
-        todayMinTemperatureLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
-        todayMinTemperatureLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
-        
-        // setup todayMaxTemperatureLabel constraints
-        todayMaxTemperatureLabel.translatesAutoresizingMaskIntoConstraints = false
-        todayMaxTemperatureLabel.topAnchor.constraint(equalTo: currentTemperatureLabel.bottomAnchor, constant: 10).isActive = true
-        todayMaxTemperatureLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -60).isActive = true
-        
-        // setup collectionView constraints
-        collectionView?.translatesAutoresizingMaskIntoConstraints = false
-        collectionView?.topAnchor.constraint(equalTo: todayDateLabel.bottomAnchor, constant: 5).isActive = true
-        collectionView?.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        collectionView?.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        collectionView?.bottomAnchor.constraint(equalTo: tableView.topAnchor).isActive = true
-        
-        // setup tableView constraints
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.topAnchor.constraint(equalTo: todayDateLabel.bottomAnchor, constant: 100).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -(bottomPadding ?? 0)).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        
         // setup manageButton constraints
         manageButton.translatesAutoresizingMaskIntoConstraints = false
-        manageButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 10).isActive = true
         manageButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
         manageButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
         // setup weatherLinkButton constraints
         weatherLinkButton.translatesAutoresizingMaskIntoConstraints = false
-        weatherLinkButton.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 10).isActive = true
         weatherLinkButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
         weatherLinkButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        weatherLinkButton.widthAnchor.constraint(equalTo: weatherLinkButton.heightAnchor).isActive = true
-        
+        weatherLinkButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        weatherLinkButton.heightAnchor.constraint(equalTo: weatherLinkButton.widthAnchor).isActive = true
     }
     
-    func success() {
-        print("")
-    }
-    
-    func failure() {
-        print("")
+    @objc func pageControlTapHaddler(sender: UIPageControl) {
+        scrollView.scrollTo(horisontalPage: sender.currentPage , animated: true)
     }
     
 }
@@ -276,5 +122,15 @@ extension MainWeatherViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageIndex = Int(round(scrollView.contentOffset.x / self.view.frame.width))
         pageControl.currentPage = pageIndex
+    }
+}
+
+extension MainWeatherViewController: MainWeatherViewProtocol {
+    func success() {
+        print("")
+    }
+    
+    func failure() {
+        print("")
     }
 }
